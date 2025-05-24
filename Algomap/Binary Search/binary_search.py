9e959ead4d50dd616947e3b73c5e0668de38d134
Ayush Mehta <mehtaayush144@gmail.com>
2025-05-24 15:34:35 -0700

    Started with Binary Search and solved first easy problem of BS.

---
+++ Algomap/Binary Search/binary_search.py
@@ -0,0 +1,35 @@
+# 704. Binary Search
+
+# Example 1:
+
+# Input: nums = [-1,0,3,5,9,12], target = 9
+# Output: 4
+# Explanation: 9 exists in nums and its index is 4
+# Example 2:
+
+# Input: nums = [-1,0,3,5,9,12], target = 2
+# Output: -1
+# Explanation: 2 does not exist in nums so return -1
+
+
+from typing import List
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = left + (right- left) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+    
+        return -1
+    
+res = Solution()
+print(res.search(nums=[-1,0,3,5,9,12], target=9))
